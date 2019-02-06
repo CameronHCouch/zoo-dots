2eda875f44603a976dca0905dafef60dab1b8e4f
CameronHCouch <cameronhcouch@gmail.com>
2019-02-06 14:26:29 -0500

    Draw initial canvas and center on page

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('hey')
+// const GameBoard = require('./game_board');
+// const Game = require('./game.js');
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("zoo-canvas");
+  canvas.width = 480;
+  canvas.height = 640;
+
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'orange';
+  ctx.strokeStyle = "black";
+  ctx.fillRect(0, 0, 480, 640);
+  ctx.clearRect(0, 0, 480, 640);
+  ctx.strokeRect(0, 0, 480, 640);
+})
+
+console.log("hiya")
